7e0e187478a6db1e8d01ff8a140aa8c0d6f8979f
Dongguo WU <david.dong.guo@gmail.com>
2023-12-19 11:40:49 -0500

    #1.5 update get assembly by namespace

--- tests/Unit/Architecture.Tests/ArchitectureTests.cs
+++ tests/Unit/Architecture.Tests/ArchitectureTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.Reflection;
 using BooksStore.Application.Books.Commands;
 using BooksStore.Domain.Entities;
 using BooksStore.Infrastructure;
@@ -9,10 +9,11 @@ namespace BooksStore.Architecture.Tests;
 [TestFixture]
 public class ArchitectureTests
 {
+    private const string SharedKernelNamespace = "SharedKernel";
     private const string DomainNamespace = "BooksStore.Domain";
     private const string ApplicationNamespace = "BooksStore.Application";
 
-    private readonly string[] InfrastructureNamespaces = new[] { "BooksStore.Infrastructure" };
+    private readonly string[] InfrastructureNamespaces = new[] { "BooksStore.Infrastructure", "BooksStore.Infrastructure.Shared" };
     private readonly string[] PresentationNamespaces = new[] { "BooksStore.Presentation" };
 
     [TestCase]
@@ -20,7 +21,6 @@ public class ArchitectureTests
     {
         // Arrange
         var assembly = typeof(Book).Assembly;
-
         var otherProjects = new[] { ApplicationNamespace }.Concat(InfrastructureNamespaces).Concat(PresentationNamespaces).ToArray() as string[];
 
         // Act
@@ -34,11 +34,16 @@ public class ArchitectureTests
     public void Domain_Should_Have_DependencyOnSharedKernel()
     {
         // Arrange
-        var assembly = typeof(Book).Assembly;
+        var domainAssembly = GetAssemblyByNamespace(DomainNamespace);
+        if (domainAssembly == null)
+        {
+            Assert.Fail($"Assembly for namespace '{DomainNamespace}' not found.");
+        }
+        var dependency = SharedKernelNamespace;
 
         // Act
-        var testResult = Types.InAssembly(assembly).That().HaveNameEndingWith("Book").Should()
-            .HaveDependencyOn("SharedKernel").GetResult();
+        var testResult = Types.InAssembly(domainAssembly).That().HaveNameEndingWith("Book").Should()
+            .HaveDependencyOn(dependency).GetResult();
 
         // Assert
         Assert.That(testResult.IsSuccessful, Is.True);
@@ -60,6 +65,20 @@ public class ArchitectureTests
         Assert.That(testResult.IsSuccessful, Is.True);
     }
 
+    [TestCase]
+    public void Application_Should_Have_DependencyOnDomain()
+    {
+        // Arrange
+        var assembly = typeof(BooksStore.Application.Books.Commands.CreateBookCommandHandler).Assembly;
+        var otherProject = DomainNamespace;
+
+        // Act
+        var testResult = Types.InAssembly(assembly).That().HaveNameEndingWith("CreateBookCommandHandler").Should()
+            .HaveDependencyOn(otherProject).GetResult();
+
+        // Assert
+        Assert.That(testResult.IsSuccessful, Is.True);
+    }
 
     [TestCase]
     public void Infrastructure_Should_Not_HaveDependencyOnOtherProjects()
@@ -78,7 +97,7 @@ public class ArchitectureTests
     }
 
     [TestCase]
-    public void Presentation_should_Not_HaveDependencyOnOtherProjects()
+    public void Presentation_Should_Not_HaveDependencyOnOtherProjects()
     {
         // Arrange
         // var assembly = typeof(BooksStore.Application.AssemblyReference).Assembly;
@@ -121,5 +140,20 @@ public class ArchitectureTests
 
         // Assert
         Assert.That(testResult.IsSuccessful, Is.True);
+    }
+
+    // Method to retrieve assembly by namespace
+    private Assembly? GetAssemblyByNamespace(string namespaceToSearch)
+    {
+        try
+        {
+            return Assembly.Load(namespaceToSearch);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error loading assembly: {ex.Message}");
+            return null;
+        }
+        //return AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(assembly => assembly.GetTypes().Any(type => type.Namespace == namespaceToSearch));
     }
 }
